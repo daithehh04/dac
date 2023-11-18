cdec46b1a0efeba7e23c8b4d3b61872a65526b48
SyxNeet <110085910+SyxNeet@users.noreply.github.com>
2023-11-18 17:56:27 +0700

    update form contact

--- src/components/contact/FormContact.js
+++ src/components/contact/FormContact.js
@@ -72,7 +72,7 @@ function FormContact({ dataForm }) {
     return (
         <section className='contactForm md:pt-[4.7rem] pt-[5.07rem]  max-md:flex-col md:pl-[4.17rem] md:pr-[9.43rem]  md:pb-[6.56rem] flex justify-between'>
             {/* content-left */}
-            <div className='max-md:px-[4.27rem]'>
+            {/* <div className='max-md:px-[4.27rem]'>
                 <h2 className='heading2 md:w-[23.4375rem] md:mb-[2.8rem] mb-[5.07rem]'>{dataForm?.heading}</h2>
                 {dataForm?.address?.map((item, index) => (
                     <div onClick={() => {
@@ -84,12 +84,12 @@ function FormContact({ dataForm }) {
                     </div>
                 ))}
 
-            </div>
+            </div> */}
 
             {/* content-right */}
 
-            <div className='max-md:mb-[10rem] max-md:px-[4.27rem]'>
-                <h3 className='description !font-normal md:mb-[3rem] max-md:!text-[3.2rem] mb-[5rem] md:w-[39.21875rem]'>{dataForm?.formData?.title}</h3>
+            <div className='max-md:mb-[10rem] max-md:px-[4.27rem] w-full'>
+
                 <Formik
                     initialValues={{ ...INITAL_FORM_STATE }}
                     validationSchema={FORM_VALIDATION}
@@ -103,45 +103,67 @@ function FormContact({ dataForm }) {
                     {({ errors, touched }) => {
                         return (
                             <Form>
-                                <div className='flex flex-col bg-transparent formSubmit md:mb-[1rem]'>
-                                    {/* họ tên */}
-                                    <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.name}</span>
-                                    <div className='flex justify-between items-center border-b border-solid border-[#000]  md:mb-[1rem]  max-md:pb-[1.5rem] max-md:pt-[1rem] ' >
-                                        <Field name="fullName" type='text' className='bg-transparent' />
-                                        <Image src={layer_11} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
-                                    </div>
-                                    {errors.fullName && touched.fullName ? (
-                                        <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]' >{errors.fullName}</div>
-                                    ) : null}
+                                <div className='flex justify-between'>
 
-                                    {/* email */}
-                                    <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.email}</span>
-                                    <div className='flex justify-between items-center border-b border-solid border-[#000] max-md:pb-[1.5rem]  md:mb-[1rem] max-md:pt-[1rem] ' >
-                                        <Field name="email" type="email" className='bg-transparent' />
-                                        <Image src={layer_12} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
+                                    <div className='max-md:px-[4.27rem]'>
+                                        <h2 className='heading2 md:w-[23.4375rem] md:mb-[2.8rem] mb-[5.07rem]'>{dataForm?.heading}</h2>
+                                        {dataForm?.address?.map((item, index) => (
+
+                                            <div key={index} className='flex items-center md:mb-[1.61rem] mb-[5rem]'>
+                                                <Field
+                                                    type='radio'
+                                                    name='address'
+                                                    value={item?.text}
+                                                />
+                                                <span className='description !font-normal md:ml-[1rem]'>
+                                                    {item?.text}
+                                                </span>
+                                            </div>
+                                        ))}
+
                                     </div>
-                                    {errors.email && touched.email ? <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]'>{errors.email}</div> : null}
-                                    {/* telephone */}
-                                    <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.telephone}</span>
-                                    <div className='flex justify-between items-center border-b border-solid border-[#000] max-md:pb-[1.5rem] md:mb-[1rem] max-md:pt-[1rem] ' >
-                                        <Field name="telephone" type='text' className='bg-transparent' />
-                                        <Image src={layer_13} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
+                                    <div className='flex flex-col bg-transparent formSubmit md:mb-[1rem]'>
+                                        <h3 className='description !font-normal md:mb-[3rem] max-md:!text-[3.2rem] mb-[5rem] md:w-[39.21875rem]'>{dataForm?.formData?.title}</h3>
+                                        {/* họ tên */}
+                                        <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.name}</span>
+                                        <div className='flex justify-between items-center border-b border-solid border-[#000]  md:mb-[1rem]  max-md:pb-[1.5rem] max-md:pt-[1rem] ' >
+                                            <Field name="fullName" type='text' className='bg-transparent' />
+                                            <Image src={layer_11} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
+                                        </div>
+                                        {errors.fullName && touched.fullName ? (
+                                            <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]' >{errors.fullName}</div>
+                                        ) : null}
+
+                                        {/* email */}
+                                        <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.email}</span>
+                                        <div className='flex justify-between items-center border-b border-solid border-[#000] max-md:pb-[1.5rem]  md:mb-[1rem] max-md:pt-[1rem] ' >
+                                            <Field name="email" type="email" className='bg-transparent' />
+                                            <Image src={layer_12} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
+                                        </div>
+                                        {errors.email && touched.email ? <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]'>{errors.email}</div> : null}
+                                        {/* telephone */}
+                                        <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[116.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.telephone}</span>
+                                        <div className='flex justify-between items-center border-b border-solid border-[#000] max-md:pb-[1.5rem] md:mb-[1rem] max-md:pt-[1rem] ' >
+                                            <Field name="telephone" type='text' className='bg-transparent' />
+                                            <Image src={layer_13} alt='icon' quality={100} width={1000} height={1000} className='md:w-[1.35417rem] md:h-[1.25rem] w-[4.9536rem] h-[4.57253rem] object-contain' />
+                                        </div>
+                                        {errors.telephone && touched.telephone ? (
+                                            <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]'>{errors.telephone}</div>
+                                        ) : null}
+                                        {/* content */}
+                                        <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[140.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.content}</span>
+                                        <div className='flex flex-col justify-between max-md:pt-[2rem] mb-[8rem] border-b border-solid  border-[#000] md:mb-[1rem]' >
+                                            <Field
+                                                className='outline-none md:w-[25rem]'
+                                                minRows={4}
+                                                name='message'
+                                            />
+                                        </div>
+                                        {/* <Field name="address" value={address} type='text' className='bg-transparent' /> */}
+                                        {/* <input name='address' className='hidden' value={address} /> */}
                                     </div>
-                                    {errors.telephone && touched.telephone ? (
-                                        <div className='md:mb-[1rem] text-[#ff0000] max-md:text-[2rem]'>{errors.telephone}</div>
-                                    ) : null}
-                                    {/* content */}
-                                    <span className='md:text-[1.04167rem] max-md:mt-[2rem] text-[4.26667rem] leading-[140.662%] md:tracking-[-0.05208rem] tracking-[-0.21333rem]'>{dataForm?.formData?.content}</span>
-                                    <div className='flex flex-col justify-between max-md:pt-[2rem] mb-[8rem] border-b border-solid  border-[#000] md:mb-[1rem]' >
-                                        <TextareaAutosize
-                                            className='outline-none md:w-[25rem]'
-                                            minRows={4}
-                                            name='message'
-                                        />
-                                    </div>
-                                    <Field name="address" type='text' />
-                                    {/* <input name='address' className='hidden' value={address} /> */}
                                 </div>
+
                                 <Button text={dataForm?.formData?.button || 'Gửi thông tin'} />
                             </Form>
                         )
