4aa0f39fa4e949bb31518ae26826dadee051ae39
Syxneet <nguyenvansy09092001@gmai.com>
2023-12-05 10:13:31 +0700

    fix autoplay video

--- src/components/home/VideoSlide.js
+++ src/components/home/VideoSlide.js
@@ -8,7 +8,8 @@ import imgVideo from '@/assets/imgs/img-video.jpg'
 import ReactPlayer from 'react-player'
 function VideoSlide({ dataSlideVideo }) {
   const [indexSlider, setIndexSlider] = useState(0)
-  const [isPlay, setIsPlay] = useState(false)
+  // const [isPlay, setIsPlay] = useState(false)
+  const [isPlay, setIsPlay] = useState(Array(dataSlideVideo.length).fill(false));
   const swiperRef = useRef()
   const handleNextSlide = () => {
     swiperRef.current?.slideNext()
@@ -19,10 +20,9 @@ function VideoSlide({ dataSlideVideo }) {
 
   const handleSlideChange = (swiper) => {
     setIndexSlider(swiper.activeIndex)
-    setIsPlay(false)
+    setIsPlay(Array(dataSlideVideo.length).fill(false));
   }
 
-  console.log(indexSlider);
   return (
     <div className='relative md:h-[100vh] h-[100rem] content slide-product mt-[8.5rem]'>
       <Swiper
@@ -46,22 +46,26 @@ function VideoSlide({ dataSlideVideo }) {
                 <div
                   className='h-full'
                   onClick={() => {
-                    setIsPlay(!isPlay)
-                    } 
-                  }
+                    const newIsPlay = [...isPlay];
+                    newIsPlay[index] = !newIsPlay[index];
+                    setIsPlay(newIsPlay);
+                 }}
                 >
-                  <Image src={item?.background?.sourceUrl || imgVideo} width={1000} height={1000} quality={100} alt='img' className={`w-full h-full absolute z-[1] inset-0 object-cover ${isPlay ? 'hidden' : ''}`} />
-                  <svg xmlns="http://www.w3.org/2000/svg" className={`md:w-[4.5rem] shadow-md md:h-[5rem] w-[10rem] h-[10rem] absolute z-[1] top-[50%] left-[50%] -translate-x-1/2 -translat-y-1/2 cursor-pointer ${isPlay ? 'hidden' : 'block'}`} viewBox="0 0 64 76" fill="none">
+                  <Image src={item?.background?.sourceUrl || imgVideo} width={1000} height={1000} quality={100} alt='img' className={`w-full h-full absolute z-[1] inset-0 object-cover ${isPlay[index] ? 'hidden' : ''}`} />
+                  <svg xmlns="http://www.w3.org/2000/svg" className={`md:w-[4.5rem] shadow-md md:h-[5rem] w-[10rem] h-[10rem] absolute z-[1] top-[50%] left-[50%] -translate-x-1/2 -translat-y-1/2 cursor-pointer ${isPlay[index] ? 'hidden' : 'block'}`} viewBox="0 0 64 76" fill="none">
                     <path d="M11.5953 1.67574C5.19164 -2.25456 0 0.965187 0 8.8615V67.1328C0 75.037 5.19164 78.2526 11.5953 74.326L59.1957 45.1168C65.6014 41.1851 65.6014 34.8152 59.1957 30.8844L11.5953 1.67574Z" fill="white" />
                   </svg>
-                  <ReactPlayer
-                    onEnded={()=>setIsPlay(false)}
-                    className={`w-full h-full video_slide_home`}  
-                    width={'100%'} 
-                    height={'100%'} 
-                    controls={true} 
-                    playing={isPlay} 
-                    url={item?.videoLink} />
+                    {isPlay[index] && (
+                      <ReactPlayer
+                         onEnded={() => setIsPlay(false)}
+                         className="w-full h-full video_slide_home"
+                         width="100%"
+                         height="100%"
+                         controls={true}
+                         playing={true}
+                         url={item?.videoLink}
+                      />
+                   )}
                   <p className={`text-white text-[6.667rem] opacity-40 font-bold uppercase absolute top-[50%] -translate-y-1/2 w-full text-center ${isPlay ? 'hidden' : ''}`}>Video app</p>
                 </div>
               )}
